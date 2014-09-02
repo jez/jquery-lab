d2cb6d0a00b2ace36b604a82c5ca0271b6a6c0ad
Jacob Zimmerman <zimmerman.jake@gmail.com>
2014-09-02 16:04:14 -0400

    Set the initial values on the "scoreboard"
    One last thing to take care of before we start going nuts with our game
    is to set up number of greens, blues, and total tiles, and then display
    these values.
    
    We'll initialize a few global variables called `reds`, `greens`, and
    `blues` to 0, set `greens` to 1 (for the upper left tile), and set
    `blues` to the remaining number of tiles. Obviously, the total number of
    tiles is `rows * columns`, and this will be important because we will
    know the user has won if the number of reds is equal to this value.
    
    Finally, we'll use a couple more jQuery calls to display these values.
    The `.html` function on a jQuery object will set the inner HTML of that
    attribute to the text specified (if you pass something that's not a
    string, it will try and cast it to a string). That means we can use this
    method, passing it `blues` and `greens` respectively, to set these
    colors to the right values. Remember that the HTML elements with id's
    `blues` and `greens` house the data we want to display. Note that we
    don't have to worry about setting the number of reds because it should
    already be set to 0 in the HTML (you can verify this).

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
     var rows;
     var cols;
 
+    var reds = 0;
+    var blues = 0;
+    var greens = 0;
+
     var boardColors = [];
+
+    var total;
 
     function play(e) {
       e.preventDefault();
@@ -37,6 +43,19 @@
 
       // Top left piece needs to start out green
       setColor(0,0,'green');
+
+      // Initialize these for displaying to the user
+      greens = 1;
+      blues = rows * cols - 1;
+
+      // Keep track of this so that we can determine if the user has won
+      total = rows * cols;
+
+      // Set the initial values 
+      // (remember that there are no reds initally, 
+      // and we are already showing 0 from our index.html file)
+      $('#blues').html(blues);
+      $('#greens').html(greens);
     }
 
     $(document).ready(function() {
